8c3cfb088adfe4a2dcdfe1716495506482a0eac7
Alex Ilves <mearepoo@gmail.com>
2020-02-08 10:43:24 -0500

    soap

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -6,5 +8,8 @@ app = Flask(__name__)
 from .views import home_view
 from .views import who_view
 from .views import hello_view
+
+
+#soap
 
 
